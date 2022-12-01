e257465fd27785e782e635171e0f8d8ba7154b33
Lev Perla <levperla@mail.ru>
2022-12-01 14:06:44 +0600

    minot fix

--- src/core/dice_metric.py
+++ src/core/dice_metric.py
@@ -7,5 +7,6 @@ def dice_coeff(y_real, y_pred):
     intersection = (y_pred * y_real).sum()
     res = (2. * intersection + smooth) / (torch.sum(y_real) + torch.sum(y_pred) + smooth)
     return res
+
 def dice_loss(y_real, y_pred):
     return 1. - dice_coeff(y_real, y_pred)
